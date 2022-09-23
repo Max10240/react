30d42331198f09926f30e1534052f558ae52e1f5
Stephen Sugden <me@stephensugden.com>
2022-09-23 11:03:07 +0200

    DO NOT MERGE: test hacks
    This allows running tests for eslint-plugin-react-hooks without having to test all of React.
    
    This commit should **not** be merged upstream!

--- packages/eslint-plugin-react-hooks/__tests__/ESLintRuleExhaustiveDeps-test.js
+++ packages/eslint-plugin-react-hooks/__tests__/ESLintRuleExhaustiveDeps-test.js
@@ -8,7 +8,7 @@
 'use strict';
 
 const ESLintTester = require('eslint').RuleTester;
-const ReactHooksESLintPlugin = require('eslint-plugin-react-hooks');
+const ReactHooksESLintPlugin = require('@grncdr/eslint-plugin-react-hooks');
 const ReactHooksESLintRule = ReactHooksESLintPlugin.rules['exhaustive-deps'];
 
 /**
